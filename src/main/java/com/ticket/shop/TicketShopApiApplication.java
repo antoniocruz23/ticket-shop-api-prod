3b8ab1a4d7391a911efcdf952e31704ef5cdec0e
Tojo <antonio.cruz@mindera.com>
2023-01-25 22:50:10 +0000

    project init

---
+++ src/main/java/com/ticket/shop/TicketShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/shop/TicketShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
