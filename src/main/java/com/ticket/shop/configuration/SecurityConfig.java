23a0796e5067ec4a4073304b2b5109e0ad2ca0a4
Tojo <antonio.cruz@mindera.com>
2023-01-25 23:07:20 +0000

    Upgrade security - removing use of deprecated class

--- src/main/java/com/ticket/shop/configuration/SecurityConfig.java
+++ src/main/java/com/ticket/shop/configuration/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.ticket.shop.configuration;
 
 import com.ticket.shop.security.UserAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final UserAuthenticationEntryPoint userAuthenticationEntryPoint;
 
@@ -20,8 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.userAuthenticationEntryPoint = userAuthenticationEntryPoint;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors()
                 .and()
@@ -37,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/v3/api-docs/*")
                 .permitAll()
                 .anyRequest().authenticated();
+
+        return http.build();
     }
 
 }
